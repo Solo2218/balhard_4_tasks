b60732137081e4300cfcd4d96464ae76190115c2
Konstantin Solovei <SoloveiKonstantin@gmail.com>
2022-01-19 16:54:42 +0300

    month_to_seazon.py complete

--- tasks/dict_tasks/month_to_seazon.py
+++ tasks/dict_tasks/month_to_seazon.py
@@ -13,6 +13,20 @@
 - month_to_season(9) -> 'Осень'
 """
 
+monrt_to_seazon = {
+    1: 'Зима',
+    2: 'Зима',
+    3: 'Весна',
+    4: 'Весна',
+    5: 'Весна',
+    6: 'Лето',
+    7: 'Лето',
+    8: 'Лето',
+    9: 'Осень',
+    10: 'Осень',
+    11: 'Осень',
+    12: 'Зима'
+}
 
 def month_to_season(month: int) -> str:
     """Возвращает сезон по его номеру
@@ -23,7 +37,8 @@ def month_to_season(month: int) -> str:
     :return: название сезона, например "зима"
     :rtype: str
     """
-    season = None
+
+    season = monrt_to_seazon.get(month)
     return season
 
 
